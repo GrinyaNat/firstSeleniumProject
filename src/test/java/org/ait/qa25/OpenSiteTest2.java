620e07009002cd5c0e8086d8e8eca27f30a5a3ff
Natalia Hrytsenia <hrytsenianatalia@gmail.com>
2023-08-26 22:08:48 +0200

    tests css and xpath

--- src/test/java/org/ait/qa25/OpenSiteTest2.java
+++ src/test/java/org/ait/qa25/OpenSiteTest2.java
@@ -1,6 +1,8 @@
 package org.ait.qa25;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -26,12 +28,37 @@ public class OpenSiteTest2 {
         System.out.println("Hello!");
     }
 
-    @AfterMethod
+    @Test
+    public void findElementByCss() {
+        driver.findElement(By.cssSelector("#dialog-notifications-success"));
+        driver.findElement(By.cssSelector("#bar-notification"));
+        driver.findElement(By.cssSelector("#dialog-notifications-error"));
+        driver.findElement(By.cssSelector("#ui-id-1"));
 
-    public void tearDown(){
-        driver.quit();
+        driver.findElement(By.cssSelector(".master-wrapper-page"));
+        driver.findElement(By.cssSelector(".master-wrapper-content"));
+        driver.findElement(By.cssSelector(".close"));
 
+        driver.findElement(By.cssSelector("[name='generator']"));
+        driver.findElement(By.cssSelector("[name='keywords']"));
+        driver.findElement(By.cssSelector("[name='description']"));
+    }
+    @Test
+    public void findElementByXpath() {
+        driver.findElement(By.xpath("//input[@id='dialog-notifications-success']"));
+        driver.findElement(By.xpath("//input[@id='bar-notification']"));
+        driver.findElement(By.xpath("//input[@id='dialog-notifications-error']"));
+        driver.findElement(By.xpath("//input[@id='ui-id-1']"));
+
+        driver.findElement(By.xpath("//label[@class='master-wrapper-page']"));
+        driver.findElement(By.xpath("//label[@class='master-wrapper-content']"));
+        driver.findElement(By.xpath("//label[@class='close']"));
+
+        driver.findElement(By.xpath("//label[@class='generator']"));
+        driver.findElement(By.xpath("//label[@class='keywords']"));
+        driver.findElement(By.xpath("//label[@class='description']"));
     }
 
-}
+        public void tearDown() { driver.quit(); }
+    }
 
